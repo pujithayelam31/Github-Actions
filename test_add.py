405c5ea7b6d53f593ed10444b2437d81df825207
pujitha <yelampallipoojitha@gmail.com>
2026-02-24 12:57:02 +0530

    code rechange-7

---
+++ test_add.py
@@ -0,0 +1,5 @@
+from example import add
+# import math
+
+def test_add():
+    assert add(2, 3) == 5
--- testing_code.py
+++
@@ -1,5 +0,0 @@
-from number import add
-# import math
-
-def testing_code():
-    assert add(2, 3) == 5
